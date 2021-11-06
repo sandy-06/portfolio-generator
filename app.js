4f41e29c70fac4996daaeacf42cb61b0caa94a29
Sandra Sanders <sandy-sanders@msn.com>
2021-11-05 20:51:12 -0700

    write file as a promise

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const generatePage = require('./src/page-template.js')
 
 const promptUser = () => {
   // destructure projects and about data from templateData based on their property key names
-  
+
   return inquirer.prompt([
     {
       type: 'input',
@@ -143,10 +143,21 @@ promptUser()
   .then(promptProject)
   .then(portfolioData => {
 
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw err;
-
-      console.log('Portfolio complete! Check out index.html to see the output!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
+
+    
+  
