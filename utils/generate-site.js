dcf6dd548acacedcfa7ec24f45138f7a1aa1d6b6
Sandra Sanders <sandy-sanders@msn.com>
2021-11-06 11:58:27 -0700

    generat-site

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const writeFile = fileContent => {
     return new Promise((resolve, reject) => {
         fs.writeFile('./dist/index.html', fileContent, err => {
-            //if ther's an error, reject the Promise and send the error to the Promise's`.catch()` method
+            //if there's an error, reject the Promise and send the error to the Promise's`.catch()` method
             if (err) {
                 reject(err);
                 //return out of the function here to make sure the Promise doesn't accidentally execute the resolve()function as well
